ea25a700b782a7a971789aaa5e4c009bc2277d82
Heiaswr <frances.matth@gmail.com>
2024-01-08 02:43:16 +0100

    NavigationContainer nested (apparently)

--- App.js
+++ App.js
@@ -1,17 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import HomeScreen from "./app/screens/HomeScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <HomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
