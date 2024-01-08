c637e70fe4837dd0aa5f3f141870b42d50e987d5
Heiaswr <frances.matth@gmail.com>
2024-01-08 03:21:42 +0100

    Login added

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function LoginScreen(props) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Login (TODO)</Text>
+    </View>
+  );
+}
+
+export default LoginScreen;
