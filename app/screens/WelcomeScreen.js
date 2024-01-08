cb8802086fe0603cc01b8a7b00e8ac057403af63
Heiaswr <frances.matth@gmail.com>
2024-01-08 03:07:38 +0100

    WelcomeScreen added 2

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import HomeScreen from "./HomeScreen";
+import RankingScreen from "./RankingScreen";
+
+function WelcomeScreen(props) {
+  const Tab = createBottomTabNavigator();
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Ranking" component={RankingScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default WelcomeScreen;
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
